d69fd622106e110995a58e29652ad35630aad713
Filip Dziedzic <dziedzicfilip00@gmail.com>
2024-11-13 15:49:56 +0100

    Dodadanie do pliku HTML

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-import { FinalCommand } from './prompt.js'; 
+import { FinalCommand } from './prompt.js';  
 import dotenv from 'dotenv';
-dotenv.config();
-
 import OpenAI from "openai";
+import { saveToFile } from './saveToFile.js'; 
+
+dotenv.config();
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -10,26 +11,29 @@ const openai = new OpenAI({
 
 const getFinalCommand = async () => {
   try {
- 
-    const finalCommand = await FinalCommand(); 
+    const finalCommand = await FinalCommand();
     
     if (!finalCommand) {
       throw new Error("finalCommand is empty or undefined.");
     }
 
-  
+ 
     const completion = await openai.chat.completions.create({
       model: "gpt-4o-mini",
       messages: [
         { role: "system", content: "You are a helpful assistant." },
         {
           role: "user",
-          content: finalCommand,  
+          content: finalCommand,
         },
       ],
     });
 
-    console.log(completion.choices[0].message);
+    const result = completion.choices[0].message.content;
+
+   
+    saveToFile(result);
+
   } catch (error) {
     console.error("Błąd w czasie wykonywania:", error);
   }
