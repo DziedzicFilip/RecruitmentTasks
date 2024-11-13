8d5594c4aaf039a6bc1106d0725a961072f82af6
Filip Dziedzic <dziedzicfilip00@gmail.com>
2024-11-13 14:09:40 +0100

    Podział na comand i app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { FinalCommand } from './prompt.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -6,13 +7,14 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
 
+
 const completion = await openai.chat.completions.create({
     model: "gpt-4o-mini",
     messages: [
         { role: "system", content: "You are a helpful assistant." },
         {
             role: "user",
-            content: "Write a haiku about recursion in programming.",
+            content: FinalCommand,
         },
     ],
 });
